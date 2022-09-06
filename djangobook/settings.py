090ed827ecc3aa06ecc78ca5bb950254833f4ac3
rlorimier <rere.rnt@gmail.com>
2022-09-06 16:06:49 +0000

    deployment commit 4th try

--- djangobook/settings.py
+++ djangobook/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-book-club.heroku.com", "localhost"]
+ALLOWED_HOSTS = ["django-book-club.herokuapp.com", "localhost"]
 
 
 # Application definition
